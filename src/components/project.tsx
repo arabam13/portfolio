5be8c064930e53fb2fe40c349e9ae0d647d00756
araba <araba.mohamed@gmail.com>
2024-03-19 13:00:18 +0100

    optimizing accessibility

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -33,13 +33,13 @@ const Project = (project: ProjectProps) => {
         <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[29rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
             <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
             <h3 className="text-2xl font-semibold">{t(`titleProject${project.index}`)}</h3>
-            <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
+            <p className="mt-2 leading-relaxed text-black dark:text-white/70">
             {t(`descriptionProject${project.index}`)}
             </p>
             <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto pt-2">
                 {project.tags.map((tag, index) => (
                 <li
-                    className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                    className="bg-black/[0.9] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
                     key={tag+index}
                 >
                     {tag}
